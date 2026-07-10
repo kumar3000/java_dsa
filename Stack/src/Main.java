512ac8dcce5c1db429de2ddbb3dfe99e44a25c58
Sidarth Santhosh Kumar <sidarthoscar15@gmail.com>
2026-07-10 16:32:49 -0500

    initial commit

---
+++ Stack/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<String> games = new Stack<String>();
+
+        games.push("Minecraft");
+        games.push("Rimworld");
+        games.push("CS2");
+        games.push("Madden");
+
+        System.out.println(games.pop());
+        games.
+    }
+}
